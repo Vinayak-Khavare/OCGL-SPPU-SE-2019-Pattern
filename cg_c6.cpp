c511638d4ad747d934fb2c0ae224c7f701a3589e
Vinayak khavare <vinayakkhavare62@gmail.com>
2023-12-12 00:20:04 +0530

    Sunrise and Sunset

--- cg_c6.cpp
+++ cg_c6.cpp
@@ -1,94 +1,45 @@
-#include <GL/glut.h>
-#include <math.h>
+#include<iostream>
+#include<graphics.h>
 
-const float PI = 3.1415926535f;
+using namespace std;
 
-// Sun position and color
-float sun_x = 0.0f;
-float sun_y = 0.0f;
-float sun_radius = 0.2f;
-float sun_color_r = 1.0f;
-float sun_color_g = 1.0f;
-float sun_color_b = 0.0f;
-
-// Sky color
-float sky_color_r = 0.0f;
-float sky_color_g = 0.5f;
-float sky_color_b = 1.0f;
-
-// Angle for sun movement
-float angle = 0.0f;
-
-// Sunrise and sunset flags
-bool sunrise = true;
-
-void drawSky() {
-  glBegin(GL_QUADS);
-    glColor3f(sky_color_r, sky_color_g, sky_color_b);
-    glVertex2f(-1.0f, 1.0f);
-    glVertex2f(-1.0f, -1.0f);
-    glVertex2f(1.0f, -1.0f);
-    glVertex2f(1.0f, 1.0f);
-  glEnd();
+int main(){
+	int gd = DETECT, gm;
+	initgraph(&gd, &gm, NULL);
+	initwindow(500, 500);
+	cleardevice();
+//	delay(100);
+	
+	int x = 250, y=200, r=20;
+	
+	while(y>125){
+		cleardevice();
+		line(0,250,125,0);
+		line(125,0,250,250);
+		line(250,250,375,0);
+		line(375,0,500,250);
+		line(0,300,500,300);
+		circle(x,y,r);
+		floodfill(x,y,WHITE);
+		y=y-5;
+		delay(150);
+	}
+	
+	while(y<250){
+		cleardevice();
+		line(0,250,125,0);
+		line(125,0,250,250);
+		line(250,250,375,0);
+		line(375,0,500,250);
+		line(0,300,500,300);
+		circle(x,y,r);
+		floodfill(x,y,WHITE);
+		y=y+5;
+		delay(150);
+	}
+	
+	closegraph();
+	getch();
+	
+	return 0;
 }
-
-void drawSun() {
-  glBegin(GL_TRIANGLE_FAN);
-    glColor3f(sun_color_r, sun_color_g, sun_color_b);
-    glVertex2f(sun_x, sun_y);
-    for (int i = 0; i <= 360; i++) {
-      float x = sun_radius * cos(i * PI / 180.0f) + sun_x;
-      float y = sun_radius * sin(i * PI / 180.0f) + sun_y;
-      glVertex2f(x, y);
-    }
-  glEnd();
-}
-
-void display() {
-  glClear(GL_COLOR_BUFFER_BIT);
-
-  drawSky();
-
-  // Update sun position and color based on angle
-  if (sunrise) {
-    angle += 0.01f;
-    sun_x = -1.0f + 2.0f * angle;
-    sun_color_r = 1.0f - angle;
-    sun_color_g = angle;
-  } else {
-    angle -= 0.01f;
-    sun_x = -1.0f + 2.0f * angle;
-    sun_color_r = angle;
-    sun_color_g = 1.0f - angle;
-  }
-
-  // Change direction after reaching top or bottom
-  if (sunrise && sun_x > 1.0f) {
-    sunrise = false;
-  } else if (!sunrise && sun_x < -1.0f) {
-    sunrise = true;
-  }
-
-  drawSun();
-
-  glutSwapBuffers();
-}
-
-void reshape(int width, int height) {
-  glViewport(0, 0, width, height);
-}
-
-int main(int argc, char* argv[]) {
-  glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-  glutInitWindowSize(800, 600);
-  glutCreateWindow("Sunrise and Sunset");
-
-  glutDisplayFunc(display);
-  glutReshapeFunc(reshape);
-
-  glutMainLoop();
-
-  return 0;
-}
-
